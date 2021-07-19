fe487d8a4a22154a9e508bd2ed8acd07ed785669
Dmitrij Svetov <bandodok@gmail.com>
2021-07-19 19:50:52 +0300

    Fix long line in gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,7 +5,8 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument("-f", "--format", default=0, help='set format of output')
+    parser.add_argument(
+        "-f", "--format", default=0, help='set format of output')
     args = parser.parse_args()
     print(args)
 
